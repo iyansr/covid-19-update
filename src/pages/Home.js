3830952a4e8cf308c795f7be614853dba949b010
Iyan Saputra <ikhyan7@gmail.com>
2020-03-09 08:27:01 +0700

    infinty scroll

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,20 +9,34 @@ const Home = () => {
 		isLoading: true,
 	})
 
+	const [dailyView, setDailyView] = useState([])
+	const [counter, setCounter] = useState(3)
+
 	const getDaily = async () => {
 		try {
 			setDaily({ ...daily, isLoading: true })
 			const response = await axios.get('https://covid19.mathdro.id/api/daily')
 			setDaily({ data: response.data, isLoading: false })
+			setDailyView(response.data.reverse().slice(0, counter))
 		} catch (error) {
 			console.log(error.response.data)
 		}
+	}
+
+	const showMore = () => {
+		setCounter(counter => (counter > daily.data.length ? daily.data.length : counter + 3))
 	}
 
 	useEffect(() => {
 		getDaily()
 		// eslint-disable-next-line
 	}, [])
+
+	useEffect(() => {
+		const ar = daily.data
+		const newarr = ar.slice(0, counter)
+		setDailyView(newarr)
+	}, [counter])
 
 	return (
 		<div className='container'>
@@ -43,9 +57,16 @@ const Home = () => {
 				<h2>Daily Update</h2>
 			</div>
 			<div className='row'>
-				{daily.isLoading ? null : daily.data.map((data, index) => <Card data={data} key={index} />)}
+				{daily.isLoading ? null : dailyView.map((data, index) => <Card data={data} key={index} />)}
 				{/* === */}
 			</div>
+			{counter <= daily.data.length ? (
+				<div className='text-center my-5'>
+					<button className='btn btn-info ml-auto text-center' onClick={showMore}>
+						Show More
+					</button>
+				</div>
+			) : null}
 		</div>
 	)
 }
